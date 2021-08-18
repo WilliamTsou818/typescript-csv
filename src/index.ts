560830fc62b127d25a13cad2f05a2a627d111547
WilliamTsou818 <cccc1589@gmail.com>
2021-08-18 15:16:41 +0800

    feat: add enum to store match results

--- src/index.ts
+++ src/index.ts
@@ -10,9 +10,17 @@ const matches = fs.readFileSync('football.csv', {
 
 let manUnitedWins = 0
 
+// enum
+enum MatchResult {
+  HomeWin = 'H',
+  AwayWin = 'A',
+  Draw = 'D'
+}
+
+
 for (let match of matches) {
-  if (match[1] === 'Man United' && match[5] === 'H') manUnitedWins++
-  if (match[2] === 'Man United' && match[5] === 'A') manUnitedWins++
+  if (match[1] === 'Man United' && match[5] === MatchResult.HomeWin) manUnitedWins++
+  if (match[2] === 'Man United' && match[5] === MatchResult.AwayWin) manUnitedWins++
 }
 
 console.log(`Man United wins ${manUnitedWins} games`)
